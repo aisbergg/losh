cc231780a7c93c2441573365ae53013a093e52e5
Andre Lehmann <aisberg@posteo.de>
2022-05-10 21:22:11 +0200

    chore: add tools file

---
+++ tools/tools.go
@@ -0,0 +1,6 @@
+//go:build tools
+// +build tools
+
+// This package contains the tool dependencies of the project.
+// https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
+package tools
