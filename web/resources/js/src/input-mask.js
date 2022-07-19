84d1a8bda0dd9612a355c68a432d6646a939adfe
Andre Lehmann <aisberg@posteo.de>
2022-07-19 23:51:50 +0200

    feat: add feature rich pagination form

--- web/resources/js/src/input-mask.js
+++ web/resources/js/src/input-mask.js
@@ -9,3 +9,12 @@ maskElementList.map(function (maskEl) {
 		lazy: maskEl.dataset['mask-visible'] === 'true'
 	})
 });
+
+var numMaskElementList = [].slice.call(document.querySelectorAll('[data-number-mask]'));
+numMaskElementList.map(function (maskEl) {
+	return new IMask(maskEl, {
+		mask: Number,
+        min: maskEl.dataset['number-mask-min'],
+        max: maskEl.dataset['number-mask-max'],
+	})
+});
