1ec6dce8130ff445bc85440079c28cb49d292cd0
Andre Lehmann <aisberg@posteo.de>
2022-08-17 23:22:37 +0200

    fix: use NFKD normalization

--- internal/lib/util/stringutil/normalize.go
+++ internal/lib/util/stringutil/normalize.go
@@ -9,10 +9,10 @@ import (
 	"golang.org/x/text/unicode/norm"
 )
 
-// NormalizeName normalizes a license name by performing a NFD normalization,
-// removing whitespaces and some other characters and lowercasing the result.
+// NormalizeName normalizes a name by performing a NFKD normalization, removing
+// whitespaces and some other characters and lowercasing the result.
 func NormalizeName(name string) string {
-	transformer := transform.Chain(norm.NFD, runes.Remove(runes.In(unicode.Mn)), runes.Remove(runes.In(unicode.White_Space)))
+	transformer := transform.Chain(norm.NFKD, runes.Remove(runes.In(unicode.Mn)), runes.Remove(runes.In(unicode.White_Space)))
 	normalized, _, _ := transform.String(transformer, name)
 	normalized = strings.ToLower(normalized)
 	return normalized
